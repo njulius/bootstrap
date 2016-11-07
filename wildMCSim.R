d98bbecf41c521a4a1a7d9add67b55681244eb43
njulius <njulius@iastate.edu>
2016-11-07 13:25:08 -0600

    Create wildMCSim.R
    extremely rushed code for MC sim of wild bootstrap algorithm

---
+++ wildMCSim.R
@@ -0,0 +1,34 @@
+wildMCSim <- function(iterations, numObs, treatRatio, trueTau, numBoots) {
+  
+  variances = rep(0, times = iterations)
+  
+  for(i in 1:iterations) {
+    # Generate Dataset
+    Z <- genData(numObs, treatRatio, trueTau)
+    
+    # This stores the treatment effect estimates from each bootstrap
+    treatEffects = rep(0, times = numBoots)
+    
+    # Get matching matrix
+    matches <- findMatches(Z)
+    
+    for(j in 1:numBoots) {
+      # Perform a wild
+      
+      newZ <- wildBootstrap(Z, matches)
+      
+      # Get bootstrapped estimator
+      treatEffects[j] <- getATE(newZ)
+    }
+    
+    variances[i] <- var(treatEffects)
+    
+    # We should now have a full vector of treatment effects
+    
+    # Progress report
+    paste("Iteration ", i, " of ", iterations)
+    
+  }
+  
+  return(variances)
+}
