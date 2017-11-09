b96290066b8ec8ff7a144a9c17fdde8f4947015b
njulius <njulius@iastate.edu>
2017-11-09 12:33:27 -0600

    Create appendUnitTET.R

---
+++ R Code/Rewrite/appendUnitTET.R
@@ -0,0 +1,51 @@
+appendUnitTET <- function(Z, matches) {
+  
+  # Split sample
+  treatedSample <- Z[which(Z[,2] == 1),]
+  controlSample <- Z[which(Z[,2] == 0),]
+  # Get N1 and N0
+  n1 <- length(treatedSample[,1])
+  n0 <- length(controlSample[,1])
+  
+  # First, I want to generate counterfactual outcomes
+  # for each treated unit.
+  
+  # To do this, I average the outcomes of all the matched
+  # control units. To do THIS, I create an intermediate
+  # matrix for convenience.
+  
+  inter <- matrix(controlSample[,3], nrow=length(controlSample[,3]), ncol=n1)
+  
+  # This matrix has N1 identical columns. Each column contains
+  # the outcome (Y) for the i'th control unit in the i'th row.
+  # Thus, if I can multiply element-wise this matrix with
+  # the matching matrix, I will end up with a matrix where all
+  # I need are the averages of ALL NONZERO elements in each
+  # column. This average in the j'th column will be the counterfactual for
+  # the j'th treated unit.
+  
+  # The number of matches to each treatment unit are
+  
+  nMatches <- colSums(matches)
+  
+  # The summed counterfactual outcomes to each treatment unit are
+  
+  summedCF <- colSums(inter * matches)
+  
+  # Thus the counterfactuals for the n1 treated units are
+  
+  counterFactuals <- summedCF / nMatches
+  
+  # This should be of length n1
+  
+  # Thus the treatment effect estimator for each treated unit is
+  
+  unitTET <- treatedSample[,3] - counterFactuals
+  
+  unitTETs <- c(unitTET, rep(0, times=n0))
+
+  Z <- cbind(Z, unitTETs)
+  
+  return(Z)
+  
+}
